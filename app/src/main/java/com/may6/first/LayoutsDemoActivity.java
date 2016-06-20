4bedc0b5055f8af36df7fb8d0dda68234ce7fe50
srikanthpragada <srikanthpragada@gmail.com>
2016-06-20 10:32:31 +0530

    20-June

---
+++ app/src/main/java/com/may6/first/LayoutsDemoActivity.java
@@ -0,0 +1,13 @@
+package com.may6.first;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutsDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layouts_demo);
+    }
+}
